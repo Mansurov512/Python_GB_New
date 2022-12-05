003cca653c667dddde4cd0bd674176525b9749a1
Mansurov Mikhail <do9jng@gmail.com>
2022-12-05 15:51:33 +0300

    Сделал 1е задание 3его ДЗ.

--- HomeWorks/03 HomeWorks/01.py
+++ HomeWorks/03 HomeWorks/01.py
@@ -1,1 +1,25 @@
 # 3.10 Вводим с клавиатуры десятичное число. Необходимо перевести его в шестнадцатиричную систему счисления.
+
+num = int(input('Write number: '))
+def to_hexadecimal(num):
+    ans = ''
+    while num > 0:
+        help = num % 16
+        if help == 10:
+            help = "A"
+        if help == 11:
+            help = "B"
+        if help == 12:
+            help = "C"
+        if help == 13:
+            help = "D"
+        if help == 14:
+            help = "E"
+        if help == 15:
+            help = "F"
+
+        ans = str(help) + ans
+        num //= 16
+    return ans
+
+print(to_hexadecimal(num))
